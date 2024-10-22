ec9b3a05b3e01d96d58108efbbc5dead00a1cbad
GabrielaKKK <gabrielakostova333@gmail.com>
2024-10-22 10:33:57 +0300

    Add create movie page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import homeControler from './controllers/homeController.js';
+import movieControler from './controllers/movieControler.js';
 
 const router = Router();
 
 router.use(homeControler);
+router.use('/movies', movieControler);
 
 export default router;
