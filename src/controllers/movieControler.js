e4bf624a13648cb2324e6f61a81bb95ae30e7f8f
GabrielaKKK <gabrielakostova333@gmail.com>
2024-10-22 11:03:07 +0300

    Create movie functionality

--- src/controllers/movieControler.js
+++ src/controllers/movieControler.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
 
+import movieService from "../services/movieService.js";
+
 const router = Router();
 
 // URL: /movies/create
 router.get('/create', (req, res) => {
     res.render('movies/create');
 });
+
+router.post('/create', async (req, res) => {
+    const movieData = req.body;
+
+    // TODO: save movie data
+    await movieService.create(movieData);
+
+    res.end();
+})
 
 export default router;
